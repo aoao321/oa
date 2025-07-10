8767a52e8b94a42c84d693861af21dcaf2389d35
Aoao <931203821@qq.com>
2025-07-10 13:27:33 +0800

    init: 初始工程

---
+++ common/service-util/src/test/java/com/aoao/serviceutil/ServiceUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aoao.serviceutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service-oa/src/main/java/com/aoao/serviceoa/ServiceOaApplication.java
@@ -0,0 +1,13 @@
+package com.aoao.serviceoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOaApplication.class, args);
+    }
+
+}
